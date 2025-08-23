122da9eb4d9a643923d1aad398234800fdd5df2e
Abhishek Luthra <abhishekluthra2005@gmail.com>
2025-08-23 00:42:49 -0400

    Revamped projects

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
-function toggleMenu () {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+document.querySelectorAll(".slideshow-container").forEach((slideshow) => {
+  let slides = slideshow.querySelectorAll(".slide");
+  let dots = slideshow.parentElement.querySelectorAll(".dot");
+  let prevArrow = slideshow.parentElement.querySelector(".prev");
+  let nextArrow = slideshow.parentElement.querySelector(".next");
+  let slideIndex = 0;
+  let autoPlay = true;
+  let interval;
+
+  function showSlide(n) {
+    slides.forEach((s, i) => {
+      s.classList.remove("active");
+      dots[i].classList.remove("active", "filling");
+    });
+    slides[n].classList.add("active");
+    dots[n].classList.add("active", "filling");
+  }
+
+  function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+  }
+
+  function prevSlide() {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide(slideIndex);
+  }
+
+  function startAuto() {
+    interval = setInterval(nextSlide, 10000);
+  }
+
+  function stopAuto() {
+    autoPlay = false;
+    clearInterval(interval);
+    dots.forEach((d) => d.classList.remove("filling"));
+  }
+
+  // Initialize
+  showSlide(slideIndex);
+  startAuto();
+
+  // Dot click
+  dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => {
+      slideIndex = i;
+      showSlide(slideIndex);
+      stopAuto(); // stop autoplay on manual click
+    });
+  });
+
+  // Arrow click
+  if (prevArrow)
+    prevArrow.addEventListener("click", () => {
+      prevSlide();
+      stopAuto();
+    });
+
+  if (nextArrow)
+    nextArrow.addEventListener("click", () => {
+      nextSlide();
+      stopAuto();
+    });
+});
